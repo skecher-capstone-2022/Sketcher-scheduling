9f675ff70dfc8a097bef9a7f6e8656e7b03f2be6
minhwan <jeff970069@gmail.com>
2022-03-12 12:28:02 +0900

    Revert "Merge branch 'master' into jmh"
    This reverts commit d418c8cd8c2d004e128d9ac8b0e02ce781a8ea6c.

--- src/main/java/sketcher/scheduling/repository/ManagerAssignScheduleRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package sketcher.scheduling.repository;
-
-import sketcher.scheduling.domain.ManagerAssignSchedule;
-
-import java.util.List;
-
-public interface ManagerAssignScheduleRepositoryCustom {
-    List<ManagerAssignSchedule> findByUserId(String id);
-}
