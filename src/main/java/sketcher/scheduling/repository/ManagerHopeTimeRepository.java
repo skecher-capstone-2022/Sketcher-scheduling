c76b38e96d53f493df653a1d0732676addd2c824
minhwan <jeff970069@gmail.com>
2022-03-12 11:33:49 +0900

    Revert "Revert "Merge branch 'master' into jmh""
    This reverts commit eec699b966a06dba1f8ab176c74a1c7ae6bd9477.

--- src/main/java/sketcher/scheduling/repository/ManagerHopeTimeRepository.java
+++ src/main/java/sketcher/scheduling/repository/ManagerHopeTimeRepository.java
@@ -1,8 +1,13 @@
 package sketcher.scheduling.repository;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sketcher.scheduling.domain.ManagerHopeTime;
 import sketcher.scheduling.domain.User;
+import sketcher.scheduling.dto.ManagerHopeTimeDto;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +15,9 @@ import java.util.Optional;
 public interface ManagerHopeTimeRepository extends JpaRepository<ManagerHopeTime , Integer> {
     List<ManagerHopeTime> findAll();
     List<ManagerHopeTime> findManagerHopeTimeByUser(User user);
+	Optional<ManagerHopeTime> findById(Integer id);
+
+	@Modifying
+    @Query("update ManagerHopeTime h set h.user=null where h.user=:user")
+    int bulkUserSetNull(@Param("user") User user);
 }
