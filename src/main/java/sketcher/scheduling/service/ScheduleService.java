f2c6ac5368c0330443af05848f9e905e0ba351a5
minhwan <jeff970069@gmail.com>
2022-03-12 11:52:53 +0900

    Revert "Merge branch 'master' into jmh"
    This reverts commit 824c21799bcd7e9191677e0527d4494fef9bfad5.

--- src/main/java/sketcher/scheduling/service/ScheduleService.java
+++ src/main/java/sketcher/scheduling/service/ScheduleService.java
@@ -1,13 +1,20 @@
 package sketcher.scheduling.service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import javafx.util.converter.LocalDateStringConverter;
+import jdk.nashorn.internal.runtime.options.Option;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sketcher.scheduling.domain.ManagerAssignSchedule;
 import sketcher.scheduling.domain.Schedule;
 import sketcher.scheduling.domain.User;
 import sketcher.scheduling.dto.ScheduleDto;
 import sketcher.scheduling.repository.ScheduleRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +24,7 @@ import java.util.Optional;
 public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
+    private final ManagerAssignScheduleService managerAssignScheduleService;
 
     @Transactional
     public Integer saveSchedule(ScheduleDto scheduleDto) {
@@ -27,7 +35,14 @@ public class ScheduleService {
         return scheduleRepository.findAll();
     }
 
-    public Optional<Schedule> findById(Integer id) {
+    public Optional<Schedule> findById(Integer id){
         return scheduleRepository.findById(id);
     }
+
+
+    @Transactional
+    public void deleteById(Integer Id){
+        scheduleRepository.deleteById(Id);
+    }
+
 }
