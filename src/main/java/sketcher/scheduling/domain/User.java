26765531cf27566a846cf792bc4f169c1a57b561
minhwan <jeff970069@gmail.com>
2022-03-12 12:06:31 +0900

    Revert "Revert "Merge branch 'master' into jmh""
    This reverts commit 8a194a04cc095b3cceeefce47930ddef8e0fd3b1.

--- src/main/java/sketcher/scheduling/domain/User.java
+++ src/main/java/sketcher/scheduling/domain/User.java
@@ -36,8 +36,8 @@ public class User implements UserDetails {
     @NotEmpty
 //    @Pattern(regexp = "^[a-zA-Z0-9]{3,12}$", message = "아이디를 3~12자로 입력해주세요. [특수문자 X]")
     private String id;
-    @Column(name = "auth_role")
 
+    @Column(name = "auth_role")
 //    @Enumerated(EnumType.STRING)
     private String authRole;
 
@@ -120,5 +120,11 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         // 계정이 사용 가능한지 확인하는 로직
         return true; // true -> 사용 가능
+    }
+
+    public void update(String username, String userTel, Character dropoutReqCheck) {
+        this.username = username;
+        this.userTel = userTel;
+        this.dropoutReqCheck = dropoutReqCheck;
     }
 }
