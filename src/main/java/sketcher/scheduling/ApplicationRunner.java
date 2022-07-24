53653670b9e7f281526cd9f333ad8d93836fab66
박태영 <ty990520@naver.com>
2022-07-24 20:43:04 +0900

    feat(modify schedule design) #40

--- src/main/java/sketcher/scheduling/ApplicationRunner.java
+++ src/main/java/sketcher/scheduling/ApplicationRunner.java
@@ -49,13 +49,16 @@ public class ApplicationRunner implements org.springframework.boot.ApplicationRu
         LocalDateTime date5 = LocalDateTime.of(2022,3,10,17,00);
         LocalDateTime date7 = LocalDateTime.of(2022,3,10,7,00);
 
-
-//        for (int i =0; i<3; i++){
+//        String names[] = {"박태영","정민환","이혜원","김희수","김민준","박서준","임도윤","정예준","박시우","정하준","서주원","유지호","성지훈","김준우","박건우","박서연","이서윤","이지우","김서현","박하은","김하은","김민서","유민서","박지민","김희철","김채원","이도현","김연우","유다은","김지원","서지원","이수빈","김예린",
+//        "이준영","박시후","김진우","정승우","박채은","채유나","김가은","박서영","윤민지","최예나","최수민","강수현","이동현","최한결","김재원","서민우","김연서","강다연","정나윤","김성현","김우빈","정지한","최예성","한나은","홍예지"};
+//
+//
+//        for (int i =0; i<names.length; i++){
 //        UserDto userA = UserDto.builder()
-//                .id("scheduleTest"+i)
+//                .id("user"+(i+1))
 //                .authRole("MANAGER")
-//                .password(new BCryptPasswordEncoder().encode("1234"))
-//                .username("스케줄 테스트"+i)
+//                .password(new BCryptPasswordEncoder().encode("12345"))
+//                .username(names[i])
 //                .userTel("010-1234-5678")
 //                .user_joinDate(LocalDateTime.now())
 //                .managerScore(5.0)
@@ -63,14 +66,26 @@ public class ApplicationRunner implements org.springframework.boot.ApplicationRu
 //        String user1 = userService.saveUser(userA);
 //        User userT = userRepository.findById(user1).get();
 //
-//            ManagerHopeTimeDto hopeA = ManagerHopeTimeDto.builder()
-//                    .start_time(0)
-//                    .finish_time(6)
-//                    .start_time(12)
-//                    .finish_time(18)
-//                    .user(userT)
-//                    .build();
-//            managerHopeTimeService.saveManagerHopeTime(hopeA);
+//            if(i<5){
+//                setHopeTime(userT, 0, 6);
+//            }else if(i<10){
+//                setHopeTime(userT, 6, 12);
+//            }else if(i<15){
+//                setHopeTime(userT, 0, 6);
+//                setHopeTime(userT, 6, 12);
+//            }else if(i<20){
+//                setHopeTime(userT, 12, 18);
+//                setHopeTime(userT, 18, 24);
+//            } else if(i<30){
+//                setHopeTime(userT, 6, 12);
+//                setHopeTime(userT, 12, 18);
+//                setHopeTime(userT, 18, 24);
+//            }else if(i>=30){
+//                setHopeTime(userT, 0, 6);
+//                setHopeTime(userT, 6, 12);
+//                setHopeTime(userT, 12, 18);
+//                setHopeTime(userT, 18, 24);
+//            }
 //        }
 
 
@@ -189,6 +204,15 @@ public class ApplicationRunner implements org.springframework.boot.ApplicationRu
 //        updateReqService.saveScheduleUpdateReq(updateReq2);
 //
 //
+    }
+
+    private void setHopeTime(User userT, int i2, int i3) {
+        ManagerHopeTimeDto hopeC = ManagerHopeTimeDto.builder()
+                .start_time(i2)
+                .finish_time(i3)
+                .user(userT)
+                .build();
+        managerHopeTimeService.saveManagerHopeTime(hopeC);
     }
 
 }
