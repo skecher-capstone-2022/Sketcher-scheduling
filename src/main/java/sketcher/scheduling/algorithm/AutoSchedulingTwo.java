be3336053630adc11ef6ffa00195e0280b3b65fa
박태영 <ty990520@naver.com>
2022-07-24 23:48:06 +0900

    feat(fix hopetime delay) #40

--- src/main/java/sketcher/scheduling/algorithm/AutoSchedulingTwo.java
+++ src/main/java/sketcher/scheduling/algorithm/AutoSchedulingTwo.java
@@ -31,7 +31,7 @@ public class AutoSchedulingTwo {
 
 
     private static int[] time = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
-    private static int[] value = {65, 0, 0, 130, 265, 222, 300, 181, 200, 294, 180, 130}; // 각각 0, 2 ,4, 6, 8, 10, 12, 14, 16, 18, 20, 22
+    private static int[] value = {65, 0, 0, 93, 195, 222, 289, 181, 124, 271, 178, 89}; // 각각 0, 2 ,4, 6, 8, 10, 12, 14, 16, 18, 20, 22
 
     private static int[] scheduleWeight;
 
