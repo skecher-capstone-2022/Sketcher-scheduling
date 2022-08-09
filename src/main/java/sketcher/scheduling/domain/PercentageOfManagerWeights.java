fa9bb311a4609b80009dda2ec21066738e6d2368
dclxxi <dclxxi@kakao.com>
2022-08-10 03:52:26 +0900

    feat(add Manager weight percent input form) #44
    - 매니저 가중치 입력 화면 추가
    - 매니저 가중치 수정 기능 추가
    - PercentageOfManagerWeights 구조 변경

--- src/main/java/sketcher/scheduling/domain/PercentageOfManagerWeights.java
+++ src/main/java/sketcher/scheduling/domain/PercentageOfManagerWeights.java
@@ -2,18 +2,40 @@ package sketcher.scheduling.domain;
 
 import lombok.*;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "Percentage_Of_Manager_Weights")
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PercentageOfManagerWeights {
 
-    @EmbeddedId
-    private PercentageOfManagerWeightsId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "id")
+    private Integer id;
 
+    @JoinColumn(name = "high")
+    private Integer high;
+
+    @JoinColumn(name = "middle")
+    private Integer middle;
+
+    @JoinColumn(name = "low")
+    private Integer low;
+
+    @Builder
+    public PercentageOfManagerWeights(Integer id, Integer high, Integer middle, Integer low) {
+        this.id = id;
+        this.high = high;
+        this.middle = middle;
+        this.low = low;
+    }
+
+    public void update(Integer high, Integer middle, Integer low) {
+        this.high = high;
+        this.middle = middle;
+        this.low = low;
+    }
 }
